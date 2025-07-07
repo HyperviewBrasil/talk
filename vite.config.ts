dfadafbbe387a63dfc2e1352d7a4ccae82e1d444
Daniel Jardim <hyperviewbrasil@gmail.com>
2025-07-07 13:56:07 -0300

    Ok
    Alteração pra mobile e server

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ export default defineConfig(({ mode }) => {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
+      server: {
+        allowedHosts: ['vendas.expert360.com.br']
+      },
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
